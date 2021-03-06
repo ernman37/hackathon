f40d4954a3daab3ae38dcaa8391fc6729e807f99
Rion Duckworth <rion.duckworth@gmail.com>
2021-03-06 10:31:01 -0700

    adding few things

---
+++ main.cc
@@ -0,0 +1,15 @@
+/*
+ *   Creator: Ernest M Duckworth IV
+ *   Date: $(date +%D) 
+ *   For:
+ *   Description:
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   cout << "Hello World!\n";
+   
+   return 0;
+}
